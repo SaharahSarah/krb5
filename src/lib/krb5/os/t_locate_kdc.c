298bfb2509b8cbb951b60cb23e55425613c422cc
Sarah Day <sarahday@mit.edu>
2016-01-14 13:11:21 -0500

    Remove port 750 from the ports that the KDC will listen on.
    The KDC was still listening on port 750 despite the fact that this
    functionality was supposed to have been removed in the past. Remove
    port 750 from the lists of UDP ports that the KDC listens on, as
    well as remove the secondary port from the KDC.

--- src/lib/krb5/os/t_locate_kdc.c
+++ src/lib/krb5/os/t_locate_kdc.c
@@ -1,4 +1,26 @@
 /* -*- mode: c; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ * Copyright 2016 Massachusetts Institute of Technology.  All Rights Reserved.
+ *
+ * Export of this software from the United States of America may
+ *   require a specific license from the United States Government.
+ *   It is the responsibility of any person or organization contemplating
+ *   export to obtain such a license before exporting.
+ *
+ * WITHIN THAT CONSTRAINT, permission to use, copy, modify, and
+ * distribute this software and its documentation for any purpose and
+ * without fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright notice and
+ * this permission notice appear in supporting documentation, and that
+ * the name of M.I.T. not be used in advertising or publicity pertaining
+ * to distribution of the software without specific, written prior
+ * permission.  Furthermore if you modify this software you must label
+ * your software as modified software and not distribute it in such a
+ * fashion that it might be confused with the original M.I.T. software.
+ * M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is" without express
+ * or implied warranty.
+ */
 #include "k5-platform.h"
 #include "port-sockets.h"
 #include <sys/types.h>
@@ -122,7 +144,7 @@ main (int argc, char *argv[])
     switch (how) {
     case LOOKUP_CONF:
         err = krb5_locate_srv_conf(ctx, &realm, "kdc", &sl,
-                                   htons(88), htons(750));
+                                   htons(88));
         break;
 
     case LOOKUP_DNS:
