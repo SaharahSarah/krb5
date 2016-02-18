946cdff4b0a3738d41d2f96030d38d72cda9fd5a
Sarah Day <sarahday@mit.edu>
2016-02-18 16:54:27 -0500

    Use LSA cache as default when a TGT cannot be retrieved from LSA cache
    When UAC is enabled and a domain user with Administrator privileges logs
    in the TGT is inaccessible.  This is because if the TGT is accessible it
    may be possible for a non-elevated user session to get a ticket with an
    unfiltered PAC and bypass the UAC.  This causes a bug where the current
    tickets in the LSA cache are all copied to the ccache except the TGT,
    which prevents a user session from getting new tickets.  In this case,
    ms2mit will now set the LSA cache to the default cache to workaround
    this issue.

--- src/windows/ms2mit/ms2mit.c
+++ src/windows/ms2mit/ms2mit.c
@@ -39,22 +39,36 @@ xusage(void)
     exit(1);
 }
 
-void
-main(
-    int argc,
-    char *argv[]
-    )
+/*
+ * Returns TRUE if the kerberos principal is the name of a Kerberos ticket
+ * service.
+ */
+static krb5_boolean
+is_tgs_principal(krb5_context kcontext, krb5_const_principal principal)
 {
-    krb5_context kcontext;
+    if (krb5_princ_size(kcontext, principal) != 2)
+        return FALSE;
+    if (data_eq_string(*krb5_princ_component(kcontext, principal, 0),
+        KRB5_TGS_NAME))
+        return TRUE;
+    else
+        return FALSE;
+}
+
+int
+main(int argc, char *argv[])
+{
+    krb5_context kcontext = NULL;
     krb5_error_code code;
-    krb5_ccache ccache=NULL;
-    krb5_ccache mslsa_ccache=NULL;
+    krb5_ccache ccache = NULL;
+    krb5_ccache mslsa_ccache = NULL;
     krb5_cc_cursor cursor;
     krb5_creds creds;
-    krb5_principal princ;
-    int initial_ticket = 0;
+    krb5_principal princ = NULL;
+    int found_tgt = 0;
     int option;
-    char * ccachestr = 0;
+    char *ccachestr = NULL;
+    krb5_ticket *tkt;
 
     prog = strrchr(argv[0], '/');
     prog = prog ? (prog + 1) : argv[0];
@@ -73,61 +87,49 @@ main(
 
     if (code = krb5_init_context(&kcontext)) {
         com_err(argv[0], code, "while initializing kerberos library");
-        exit(1);
+        goto cleanup;
     }
 
     if (code = krb5_cc_resolve(kcontext, "MSLSA:", &mslsa_ccache)) {
         com_err(argv[0], code, "while opening MS LSA ccache");
-        krb5_free_context(kcontext);
-        exit(1);
-    }
-
-    if (code = krb5_cc_set_flags(kcontext, mslsa_ccache, KRB5_TC_NOTICKET)) {
-        com_err(argv[0], code, "while setting KRB5_TC_NOTICKET flag");
-        krb5_cc_close(kcontext, mslsa_ccache);
-        krb5_free_context(kcontext);
-        exit(1);
+        goto cleanup;
     }
 
     /* Enumerate tickets from cache looking for an initial ticket */
     if ((code = krb5_cc_start_seq_get(kcontext, mslsa_ccache, &cursor))) {
         com_err(argv[0], code, "while initiating the cred sequence of MS LSA ccache");
-        krb5_cc_close(kcontext, mslsa_ccache);
-        krb5_free_context(kcontext);
-        exit(1);
+        goto cleanup;
     }
 
-    while (!(code = krb5_cc_next_cred(kcontext, mslsa_ccache, &cursor, &creds)))
-    {
-        if ( creds.ticket_flags & TKT_FLG_INITIAL ) {
-            krb5_free_cred_contents(kcontext, &creds);
-            initial_ticket = 1;
+    while (!found_tgt) {
+        code = krb5_cc_next_cred(kcontext, mslsa_ccache, &cursor, &creds);
+        if (code)
             break;
-        }
+
+        /* Check if the ticket is a TGT */
+        if (is_tgs_principal(kcontext, creds.server))
+            found_tgt = 1;
+
         krb5_free_cred_contents(kcontext, &creds);
     }
     krb5_cc_end_seq_get(kcontext, mslsa_ccache, &cursor);
 
     if (code = krb5_cc_set_flags(kcontext, mslsa_ccache, 0)) {
         com_err(argv[0], code, "while clearing flags");
-        krb5_cc_close(kcontext, mslsa_ccache);
-        krb5_free_context(kcontext);
-        exit(1);
+        goto cleanup;
     }
 
-    if ( !initial_ticket ) {
+    if (!found_tgt) {
         fprintf(stderr, "%s: Initial Ticket Getting Tickets are not available from the MS LSA\n",
                 argv[0]);
-        krb5_cc_close(kcontext, mslsa_ccache);
-        krb5_free_context(kcontext);
-        exit(1);
+        krb5int_cc_user_set_default_name(kcontext, "MSLSA:");
+        code = 1;
+        goto cleanup;
     }
 
     if (code = krb5_cc_get_principal(kcontext, mslsa_ccache, &princ)) {
         com_err(argv[0], code, "while obtaining MS LSA principal");
-        krb5_cc_close(kcontext, mslsa_ccache);
-        krb5_free_context(kcontext);
-        exit(1);
+        goto cleanup;
     }
 
     if (ccachestr)
@@ -136,32 +138,28 @@ main(
         code = krb5_cc_default(kcontext, &ccache);
     if (code) {
         com_err(argv[0], code, "while getting default ccache");
-        krb5_free_principal(kcontext, princ);
-        krb5_cc_close(kcontext, mslsa_ccache);
-        krb5_free_context(kcontext);
-        exit(1);
+        ccache = NULL;
+        goto cleanup;
     }
     if (code = krb5_cc_initialize(kcontext, ccache, princ)) {
         com_err (argv[0], code, "when initializing ccache");
-        krb5_free_principal(kcontext, princ);
-        krb5_cc_close(kcontext, mslsa_ccache);
-        krb5_cc_close(kcontext, ccache);
-        krb5_free_context(kcontext);
-        exit(1);
+        goto cleanup;
     }
 
     if (code = krb5_cc_copy_creds(kcontext, mslsa_ccache, ccache)) {
         com_err (argv[0], code, "while copying MS LSA ccache to default ccache");
-        krb5_free_principal(kcontext, princ);
-        krb5_cc_close(kcontext, ccache);
-        krb5_cc_close(kcontext, mslsa_ccache);
-        krb5_free_context(kcontext);
-        exit(1);
+        goto cleanup;
     }
 
-    krb5_free_principal(kcontext, princ);
-    krb5_cc_close(kcontext, ccache);
-    krb5_cc_close(kcontext, mslsa_ccache);
-    krb5_free_context(kcontext);
-    return(0);
+cleanup:
+
+    if (princ != NULL)
+        krb5_free_principal(kcontext, princ);
+    if (ccache != NULL)
+        krb5_cc_close(kcontext, ccache);
+    if (mslsa_ccache != NULL)
+        krb5_cc_close(kcontext, mslsa_ccache);
+    if (kcontext != NULL)
+        krb5_free_context(kcontext);
+    return code;
 }
